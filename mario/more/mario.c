a0877a4ae18f8309e6ca67f122f8a464f4afdef2
azarashin <azarashin1979@gmail.com>
2020-05-13 18:53:08 +0900

    Reduced nesting.

--- mario/more/mario.c
+++ mario/more/mario.c
@@ -10,30 +10,19 @@ int main(void)
         {
             for (int i = 1; i <= number; i++)
             {
-                // [notice] このループは無くして、ネストを浅くする方がよいです。
-                // k==0 の時とk==1 の時とで場合分けしているだけで、ループに意味がないため。
-                for (int k = 0; k < 2; k++)
-                {
-                    if (k == 0)
-                    {
-                        for (int j = 0; j < (number - i); j ++)
-                        {
-                            printf(" ");
-                        }
-                        for (int j = 0; j < i; j ++)
-                        {
-                            printf("#");
-                        }
-                        printf("  ");
-                    }
-                    else
-                    {
-                        for (int j = 0; j < i; j ++)
-                        {
-                            printf("#");
-                        }
-                    }
-                }
+				for (int j = 0; j < (number - i); j ++)
+				{
+					printf(" ");
+				}
+				for (int j = 0; j < i; j ++)
+				{
+					printf("#");
+				}
+				printf("  ");
+				for (int j = 0; j < i; j ++)
+				{
+					printf("#");
+				}
                 printf("\n");
             }
             return 0;
