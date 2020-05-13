36c567e450195dc367255d899763fc8084ce161b
azarashin <azarashin1979@gmail.com>
2020-05-14 07:32:42 +0900

    reviewed then modified.

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -2,76 +2,48 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int my_atoi(char *str);
 
 int main(int argc, char *argv[])
 {
-	// [notice] argc != 2でいいんじゃ…（以下略）
-    if (argc > 2 || argc <= 1)
+    if (argc != 2)
     {
         printf("Usage: ./substitution key\n");
         return 1;
     }
 
     char *keys = argv[1];
-    int keyNums[26];
-	// [notice] バッファオーバーランを起こすので、事前に文字列長を
-	// チェックしておいた方が良いです。
-	// (今回の場合はkeys に対しては読み込みアクセスだけですので、
-	// メモリを破壊することはないとは思いますが、チェックはしておいた方が良いです。)
-    for (int i = 0; i < 26; i++)
-    {
-		// [notice] 大文字と小文字を区別していますが、keyNums を求めるためだけに区別
-		// する必要はないですので、事前にすべて大文字または小文字に変換すると
-		// 少しすっきりします。
-		// 大文字・小文字への変換は標準関数で用意されています。
-		// toupper, tolower 関数です。
-        if (keys[i] >= 'a' && keys[i] <= 'z')
-        {
-            keyNums[i] = keys[i] - ('a' + i);
-        }
-        else if (keys[i] >= 'A' && keys[i] <= 'Z')
-        {
-            keyNums[i] = keys[i] - ('A' + i);
-        }
-        else
-        {
-            printf("Key must contain 26 characters.\n");
-            return 1;
-        }
-        for (int j = 0; j < 26; j++)
-        {
-            if (keys[i] == keys[j] && i != j)
-            {
-                printf("Duplicate characters in key.\n");
-                return 1;
-            }
-        }
-    }
+	if(strlen(keys) != 26) {
+		printf("Key must contain 26 characters.\n");
+		return -1; 
+	}
 
-    char *text = get_string("plaintext: ");
-    printf("ciphertext:");
-    for (int i = 0; text[i] != '\0'; i++)
-    {
-		// [notice] 単純にアルファベットから他のアルファベットへのマップであれば
-		// もう少し短く書けるのではないかと思います。
-		// 配列を上手に使いましょう。
-        if (text[i] >= 'a' && text[i] <= 'z')
-        {
-            int index = text[i] - 'a';
-            printf("%c", text[i] + keyNums[index]);
-        }
-        else if (text[i] >= 'A' && text[i] <= 'Z')
-        {
-            int index = text[i] - 'A';
-            printf("%c", text[i] + keyNums[index]);
-        }
-        else
-        {
-            printf("%c", text[i]);
-        }
-    }
-    printf("\n");
+	// 文字コードx -> x へのマップ。
+	// 但し、アルファベット文字の場合はkeys に合わせてマップ先を変更する。
+	char convert_map[256]; 
+	for(int i=0;i<256;i++) {
+		char c = (char)i;
+		int index; 
+		if(c >= 'a' && c <= 'z') {
+			// index: keys のどこを参照するかを示す
+			index = (int)(c - 'a'); 
+			convert_map[i] = tolower(keys[index]); 
+		} else if(c >= 'A' && c <= 'Z') {
+			// index: keys のどこを参照するかを示す
+			index = (int)(c - 'A');
+			convert_map[i] = toupper(keys[index]);
+		} else {
+			convert_map[i] = c; 
+		}
+	}
+
+	char *text = get_string("plaintext: ");
+	// 実際に変換処理を行う。
+	for(int i = 0;text[i] != '\0';i++) {
+		int index = (int)text[i]; // 1バイトデータなので、index は0～255 になる。
+		printf("%c", convert_map[index]); 
+	}
     return 0;
 }
