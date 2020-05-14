7657e093ea1afa3fafa11417d7e6a65ae9faafec
azarashin <azarashin1979@gmail.com>
2020-05-15 08:05:25 +0900

    reviewed then modified again.
    (this modification will not receive negative value as command line parameter.
    if we make it possible also to receive negative value,
    this code need to be modified much more.)

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,20 +3,21 @@
 #include <stdio.h>
 #include <string.h>
 
-int my_atoi(char *str);
+int my_atoi(const char *str, int* out);
 
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        printf("Usage: ./caesar key\n");
+        printf("Usage: ./caesar key(zero or positive integer)\n");
         return 1;
     }
 
-	int key = my_atoi(argv[1]); 
-    if (key == -1) 
+	int key, ret; 
+	ret = my_atoi(argv[1], &key); 
+    if (ret == -1) 
     {
-        printf("Usage: ./caesar key\n");
+        printf("Usage: ./caesar key(zero or positive integer)\n");
         return 1;
     }
 
@@ -41,36 +42,19 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-// [notice]
-// 1. 変換元の文字列は変更されないことを明示すべきです。つまり、
-// char *str ではなく、const char* str とすべきです。
-// 
-// 2. 読み込みがうまくいったかどうかの結果と、実際に読み込んだ結果とを
-// 別々に取得する方が拡張性が高いです。具体的には
-//
-// int my_atoi(const char* str, int* out)
-//
-// のように定義します。
-//
-// 3. int を返すだけの関数だと、負の数を取得できないため。
-// 今回の場合、正の整数か０のみを取得するようにすれば実装はシンプルになります。
-// 負の数の場合、符号部分があるため、若干面倒です。
-// 本気でやるなら正規表現を使いますが、今回のプログラムの本質からは
-// 外れるのではないかと思います。
-int my_atoi(char *str)	
+int my_atoi(const char *str, int* out)	
 {	
 	for (int i = 0; str[i] != '\0'; i++)	
 	{	
-		// [notice]
-		// ここの意図が不明です。&str[i] はchar のアドレス型なので、
-		// 部分文字列を示しています。つまり、部分文字列を徐々に先頭から
-		// 縮小していきながら、変換処理＆比較と、"0" との比較処理と、
-		// それぞれ実行していっています。
-		if (atoi(&str[i]) == 0 && strcmp(&str[i], "0") != 0)	
-		{	
+		// 数字以外が含まれていたら-1 を返す。
+		if (str[i] < '0' || str[i] > '9')	
+		{
+			*out = 0; // 変換できないので未定義になりますが、とりあえず0 を入れておく。
 			return -1;	
 		}	
-	}	
-	return atoi(str);	
+	}
+	// 数字のみが渡されていたら、数値に変換する。
+	*out = atoi(str);
+	return 0; 
 } 
 
