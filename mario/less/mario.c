a9689798628777518d0df041ab159d6a404d9e13
azarashin <azarashin1979@gmail.com>
2020-05-15 08:38:06 +0900

    modified to follow pset.

--- mario/less/mario.c
+++ mario/less/mario.c
@@ -22,9 +22,9 @@ int main(void)
                 printf("\n");
             }
             return 0;
-        } else {
-			printf("height must be 1 to 8. your input is %d\n", number);
+//        } else {
+//			printf("height must be 1 to 8. your input is %d\n", number);
 		}
-		return -1;
     }
+	return 0; 
 }
