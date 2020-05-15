109b55a73f565271906b2f5f6a1c7c943fde9c1b
azarashin <azarashin1979@gmail.com>
2020-05-15 21:16:51 +0900

    fixed code to follow spec. "If the key is invalid (as by not containing 26 characters, containing any character that is not an alphabetic character, or not containing each letter exactly once), your program should print an error"

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -15,9 +15,28 @@ int main(int argc, char *argv[])
     }
 
     char *keys = argv[1];
+
+	// 引数文字列(keys)の正当性チェック
 	if(strlen(keys) != 26) {
 		printf("Key must contain 26 characters.\n");
 		return -1; 
+	}
+
+	for(int i=0;i<26;i++) {
+		// 大文字・小文字を区別しないので小文字に統一して判定する
+		char key = tolower(keys[i]);
+		if(key < 'a' || key > 'z') {
+			printf("The key %c is not alphabet. \n", key); 
+			return 1; 
+		}
+
+		for(int j=0;j<26;j++) {
+			char key2 = tolower(keys[j]); 
+			if(i != j && key == key2) {
+				printf("Duplicate characters in key.\n");
+				return 1; 
+			}
+		}
 	}
 
 	// 文字コードx -> x へのマップ。
