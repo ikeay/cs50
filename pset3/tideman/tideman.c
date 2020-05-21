691cca950e27758e830bd51d07a8ccf9b1640508
azarashin <azarashin1979@gmail.com>
2020-05-21 23:06:26 +0900

    bug fixed... orz

--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -116,9 +116,10 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
+	// zero clear memory for preferences. 
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = i + 1; j < candidate_count; j++)
+        for (int j = 0; j < candidate_count; j++)
         {
             preferences[i][j] = 0;
         }
