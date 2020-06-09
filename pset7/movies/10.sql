2e6497061175026707663cc8e904e6ac20c93b8b
azarashin <azarashin1979@gmail.com>
2020-06-09 14:03:16 +0000

    added comments.

--- pset7/movies/10.sql
+++ pset7/movies/10.sql
@@ -1,3 +1,10 @@
+/*
+[note]
+INNER JOIN movies ON directors.movie_id = movies.id
+の部分は必要でしょうか？
+movies テーブルに存在しない映画を意図的に排除しようとしているのでしょうか？（そうかもしれない…）
+*/
+
 SELECT DISTINCT people.name
 FROM people
 INNER JOIN directors ON people.id = directors.person_id
--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -1,9 +1,18 @@
+/*
+[note]
+LEFT OUTER JOIN stars
+
+のところを外部結合にしたのはどういう意図でしょう？
+INNER JOIN stars
+
+で十分かと思いました。
+*/
 SELECT title
 FROM movies
 LEFT OUTER JOIN stars
 ON movies.id = stars.movie_id
 INNER JOIN people
 ON people.id = stars.person_id
-WHERE name = "Johnny Depp" OR name = "Helena Bonham Carter"
+WHERE name = "Johnny Depp" OR name = "Helena Bonham Carter" -- [note] 折角他は丁寧に書いてるので(以下略)
 GROUP BY title
 HAVING COUNT(title) = 2;
