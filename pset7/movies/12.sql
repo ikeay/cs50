ef30b6d11b4674709d976d02b04b3399a875131e
Ayaka Ikezawa <hello@ikeay.net>
2020-08-07 17:08:48 +0900

    Update 12.sql

--- pset7/movies/12.sql
+++ pset7/movies/12.sql
@@ -1,15 +1,6 @@
-/*
-[note]
-LEFT OUTER JOIN stars
-
-のところを外部結合にしたのはどういう意図でしょう？
-INNER JOIN stars
-
-で十分かと思いました。
-*/
 SELECT title
 FROM movies
-LEFT OUTER JOIN stars
+INNER JOIN stars
 ON movies.id = stars.movie_id
 INNER JOIN people
 ON people.id = stars.person_id
